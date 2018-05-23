b40413d0cb566e152aa305b71d9b4c3b79b470c1
metalmaninfr <alex_blandford@hotmail.com>
2018-05-23 16:07:31 +0200

    added article pafes

--- dist/src.be7f595a.js
+++ dist/src.be7f595a.js
@@ -98,7 +98,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   // Override the current require with this new one
   return newRequire;
-})({28:[function(require,module,exports) {
+})({13:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -128,7 +128,7 @@ function getBaseURL(url) {
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],27:[function(require,module,exports) {
+},{}],11:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -159,25 +159,25 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":28}],25:[function(require,module,exports) {
+},{"./bundle-url":13}],10:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":27}],26:[function(require,module,exports) {
+},{"_css_loader":11}],9:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":27}],24:[function(require,module,exports) {
+},{"_css_loader":11}],7:[function(require,module,exports) {
 'use strict';
 
 require('flexboxgrid');
 
 require('./styles/styles.scss');
-},{"flexboxgrid":25,"./styles/styles.scss":26}],30:[function(require,module,exports) {
+},{"flexboxgrid":10,"./styles/styles.scss":9}],23:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -206,7 +206,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '49623' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '57815' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -347,5 +347,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[30,24], null)
+},{}]},{},[23,7], null)
 //# sourceMappingURL=/src.be7f595a.map
